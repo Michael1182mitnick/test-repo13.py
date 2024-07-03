c430169c9a34780e9be93b1e67e6aefac119074c
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-07-02 17:43:31 -0800

    Create test-repo13.py

---
+++ test-repo13.py
@@ -0,0 +1,14 @@
+# example of while loop in python
+
+i = 1
+# while i <= 5:
+#     print(i)
+#     i = i + 1
+
+# while i <= 1_000:
+#     print(i)
+#     i = i + 1
+
+while i <= 10:
+    print(i * '*')
+    i = i + 1
